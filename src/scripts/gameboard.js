831f8ec5160e54cd98918d0cc62d2bf5b5abb17d
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-08-06 21:02:19 +0530

    Finish writing placeShip tests and its scripts

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,3 +1,51 @@
-class Gameboard {}
+import Ship from './ships.js';
+
+// Isolating dependency
+const createShip = (length) => new Ship(length);
+
+class Gameboard {
+  #board = new Array(10).fill(null).map(() => new Array(10).fill(null));
+
+  #setCell([x, y], value) {
+    this.#board[x][y] = value;
+  }
+
+  static #generateCoordinates(startCoordinate, shipLength, isVertical) {
+    const coordinates = [];
+    let [x, y] = startCoordinate;
+
+    for (let i = 0; i < shipLength; i++) {
+      coordinates.push([x, y]);
+      if (isVertical) {
+        y += 1;
+      } else {
+        x += 1;
+      }
+    }
+
+    return coordinates;
+  }
+
+  getCell([x, y]) {
+    if (x < 0 || y < 0 || x > 9 || y > 9)
+      throw new Error('Coordinate goes out of bounds');
+    return this.#board[x][y];
+  }
+
+  placeShip({ coordinate, shipLength = 1, isVertical = false }) {
+    const shipParts = Gameboard.#generateCoordinates(
+      coordinate,
+      shipLength,
+      isVertical,
+    );
+
+    shipParts.forEach((position) => {
+      if (this.getCell(position)) throw new Error("Ships can't overlap");
+    });
+
+    const ship = createShip(shipLength);
+    shipParts.forEach((position) => this.#setCell(position, ship));
+  }
+}
 
 export default Gameboard;
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,10 +1,73 @@
 import Gameboard from '../scripts/gameboard.js';
+import Ship from '../scripts/ships.js';
 
-test('Should be able to place ships at specific coordinates', () => {
-  const gameboard = new Gameboard();
-  gameboard.placeShip({ startCell: [5, 4], shipLength: 3, isVertical: true });
+function isShip(cell) {
+  return cell instanceof Ship;
+}
 
-  const placedShip = gameboard.getAt([5, 5]);
-  expect(placedShip.constructor.name).toBe('Ship');
-  expect(placedShip.getLength()).toBe(3);
+let gameboard;
+describe('Placing ships', () => {
+  beforeEach(() => {
+    gameboard = new Gameboard();
+  });
+
+  test('Should be able to place ships at specific coordinates', () => {
+    const coordinate = [5, 5];
+
+    gameboard.placeShip({ coordinate });
+
+    const placedShip = gameboard.getCell(coordinate);
+
+    expect(isShip(placedShip)).toBe(true);
+  });
+
+  test('Should place ships in multiple cells if length > 1', () => {
+    gameboard.placeShip({ coordinate: [0, 0], shipLength: 3 });
+
+    expect(isShip(gameboard.getCell([0, 0]))).toBe(true);
+    expect(isShip(gameboard.getCell([1, 0]))).toBe(true);
+    expect(isShip(gameboard.getCell([2, 0]))).toBe(true);
+  });
+
+  test('Should work for vertical alignment', () => {
+    gameboard.placeShip({
+      coordinate: [9, 0],
+      shipLength: 3,
+      isVertical: true,
+    });
+
+    expect(isShip(gameboard.getCell([9, 0]))).toBe(true);
+    expect(isShip(gameboard.getCell([9, 1]))).toBe(true);
+    expect(isShip(gameboard.getCell([9, 2]))).toBe(true);
+  });
+
+  test("Shouldn't be able to place out of bounds", () => {
+    expect(() => gameboard.placeShip({ coordinate: [10, 10] })).toThrow(
+      'Coordinate goes out of bounds',
+    );
+  });
+
+  test("Ship parts can't go out of bounds either", () => {
+    expect(() =>
+      gameboard.placeShip({ coordinate: [9, 9], shipLength: 2 }),
+    ).toThrow('Coordinate goes out of bounds');
+  });
+
+  test("Ships shouldn't be able to overlap each other", () => {
+    gameboard.placeShip({ coordinate: [0, 0] });
+    expect(() => gameboard.placeShip({ coordinate: [0, 0] })).toThrow(
+      "Ships can't overlap",
+    );
+  });
+
+  test("Ships' parts can't overlap either", () => {
+    gameboard.placeShip({ coordinate: [0, 1], shipLength: 3 });
+    expect(() =>
+      gameboard.placeShip({
+        coordinate: [0, 0],
+        shipLength: 3,
+        isVertical: true,
+      }),
+    ).toThrow("Ships can't overlap");
+  });
 });
